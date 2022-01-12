fc7ed25813dcac377b56661babfb798ad446c0a7
Hell Corp <corporation.h3ll@gmail.com>
2022-01-12 18:10:17 +0530

    backup

--- contracts/chesSR/ChesSR.sol
+++ contracts/chesSR/ChesSR.sol
@@ -1,45 +1,76 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.7;
+import "hardhat/console.sol";
 
 contract ChesSR {
-    // defining a owner
-    address public owner;
 
-    // making a constructor payable makes contract eligible to hold ether
+    address public owner;
+    mapping(string => Room) public contests;
+    mapping(string => bool) public roomIdExistence;
+    string[] public roomIds;
+
     constructor() payable{
-        //following line is owner is the same address which deploying on blockchain
         owner = msg.sender;
     }
 
-    //change owner. FYI put a condition so that only owner himself can assign someone as owner
     function changeOwner(address newOwner) public {
-        //checking that this method or task can be done by owner only
         require(msg.sender == owner, "Only Owner can change owner !!!");
         owner = newOwner;
     }
 
-    //modifier are like sub function
-    modifier minAmountCheck(){
-        //setting a condition that bet should be more than 99 wei.
-        require(msg.value >= 99 wei);
-        //following line must be written in the end in modifier (which kinda of tells it's modifier)
+    struct Room {
+        string roomId;
+        uint256 betAmount;
+        address payable[] players;
+    }
+
+    function getRoomList() public view returns (Room[] memory){
+        Room[] memory rooms ;
+        for (uint i=0;i<roomIds.length; i++){
+            string memory roomId = roomIds[i];
+            rooms[i]= contests[roomId];
+        }
+        return rooms;
+    }
+
+    modifier creatingRoom(string memory roomId){
+        //require(msg.value >= 100 wei);
+        Room storage room = contests[roomId];
+        room.roomId = roomId;
+
+        if(room.players.length < 2){
+            room.players.push(payable(msg.sender));
+            if(room.players.length == 1){
+                room.betAmount = msg.value;
+            }else {
+                if(msg.value == room.betAmount){
+
+                } else{
+                    //revert("Bet amount is not same");
+                }
+            }
+        } else{
+            revert("Room is already filled");
+        }
+        contests[roomId] =room;
+        if(!roomIdExistence[roomId]){
+            roomIds.push(roomId);
+            roomIdExistence[roomId] = true;
+        }
         _;
     }
 
-    //deposit funds to contract. Calling this method automatically deposit ether in to contract
-    //whatever value we give it automatically goes in msg.value and calling this method makes the deeposit
-    function deposit() external payable minAmountCheck() returns(bool){
+
+    function deposit(string memory roomId) external payable creatingRoom(roomId) returns (bool){
         return true;
     }
 
-    //funds transfer to specific address from contract
     function transfer(address payable receiver, uint256 amount) public returns (bool){
-        //checking if transfer funds are not greater than contract funds
         require(address(this).balance >= amount, "Insufficient funds");
         receiver.transfer(amount);
         return true;
     }
 
-    //Check contract balance
     function checkBalance() public view returns (uint256){
         return (address(this).balance);
     }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,17 +1,17 @@
 //1. PZCoin
-/*const PZCoin = artifacts.require("PZCoin");
+const PZCoin = artifacts.require("PZCoin");
 
 module.exports = function (deployer) {
   deployer.deploy(PZCoin);
-};*/
+};
 
 
 //2. ChesSROld
-/*const ChesSR_Old = artifacts.require("ChesSR_Old");
+const ChesSR_Old = artifacts.require("ChesSR_Old");
 
 module.exports = function (deployer) {
   deployer.deploy(ChesSR_Old);
-};*/
+};
 
 
 //3. ChesSR
